762f0c033260c2f93d7d4809008147a92b1a20b9
mukhiarambhu <rambhumukhia@gmail.com>
2022-03-19 13:45:15 +0530

    types in typescript

---
+++ typesinTypeScript/app.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+//1. number
+var b = 10; //in typescript defining types is optional ., but if we define and try to put another type it will through error,for decimal we use number only
+console.log(b);
+//2 string
+var temp = "hello ts";
+console.log(temp);
+//3 boolean
+var flag = true;
+console.log(flag);
---
+++ typesinTypeScript/app.ts
@@ -0,0 +1,16 @@
+// we can decleare type using this synatx
+// let x:number=10 it signify x is number
+export{} // to prevent duplicate idetifier error
+//1. number
+
+let b: number = 10  //in typescript defining types is optional ., but if we define and try to put another type it will through error,for decimal we use number only
+console.log(b)
+
+//2 string
+let temp: string = "hello ts"
+console.log(temp)
+
+//3 boolean
+
+let flag:boolean=true
+console.log(flag)
