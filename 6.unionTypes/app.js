a768a64531c2e93e864fe5b53e54ca34629501f1
mukhiarambhu <rambhumukhia@gmail.com>
2022-03-21 12:31:18 +0530

    uniontype in typescript

---
+++ 6.unionTypes/app.js
@@ -0,0 +1,8 @@
+//uniontype- defining more than one type to variable is known as uniontype
+var temp = 0;
+console.log(temp);
+temp = "Rambhu";
+console.log(temp);
+temp = false;
+console.log(temp);
+//here we have defined moe than one types to temp variable this is uniontype
---
+++ 6.unionTypes/app.ts
@@ -0,0 +1,10 @@
+//uniontype- defining more than one type to variable is known as uniontype
+
+let temp: number | string | boolean = 0
+console.log(temp)
+temp = "Rambhu"
+console.log(temp)
+temp = false
+console.log(temp)
+
+//here we have defined moe than one types to temp variable this is uniontype
