da71e3c834c35a67c48419b81c4af374a0789680
mukhiarambhu <rambhumukhia@gmail.com>
2022-03-22 13:05:11 +0530

    inheritance in typescript

---
+++ 10.inheritance in typescript/app.ts
@@ -0,0 +1,21 @@
+class Parent{
+    names:string; //setiing type of parent property
+   setName(name:string):void{ //as this method isnot returning anything type is void
+    this.names=name
+    }
+}
+
+class Child extends Parent{
+    age;
+     constructor(age: number) {
+        super()
+        this.age=age
+    }
+    getName():string {
+        return `name is ${this.names} and age is ${this.age}`
+    }
+}
+
+let c1 = new Child(16)
+c1.setName("rambhu")
+console.log(c1.getName())
