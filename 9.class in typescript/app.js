7bcb403907774b5ad8cd7559b426cd4c570d06cc
mukhiarambhu <rambhumukhia@gmail.com>
2022-03-22 12:11:45 +0530

    class in typescript

---
+++ 9.class in typescript/app.js
@@ -0,0 +1,23 @@
+// how to define type of property
+var user = /** @class */ (function () {
+    function user(name) {
+        this.name = name;
+    }
+    return user;
+}());
+var user1 = new user('Rambhu');
+console.log(user1);
+//how to define type of method in class
+var member = /** @class */ (function () {
+    function member(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    member.prototype.getDetails = function () {
+        return "Name of member is ".concat(this.name, " and age is ").concat(this.age);
+    };
+    return member;
+}());
+var member1 = new member("rambhu", 45);
+console.log(member1);
+console.log(member1.getDetails());
---
+++ 9.class in typescript/app.ts
@@ -0,0 +1,29 @@
+// how to define type of property
+
+class user{
+    name: string //property name should be of string only
+    constructor(name:string ) { //parameter which is passed should be of string only
+        this.name=name
+    }
+}
+
+let user1 = new user('Rambhu')
+console.log(user1)
+
+//how to define type of method in class
+
+class member{
+    name: string
+    age:number
+    constructor(name:string,age:number) {
+        this.name = name
+        this.age=age
+    }
+    getDetails():string { //if we donot return rather consoling it type will of void
+        return `Name of member is ${this.name} and age is ${this.age}`
+    }
+}
+
+let member1 = new member("rambhu", 45)
+console.log(member1)
+console.log(member1.getDetails())
